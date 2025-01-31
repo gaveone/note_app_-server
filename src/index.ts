ca9c6d37bf4ea9421cad3182fcbb612454e26dbe
“gave” <adeun2020@gmail.com>
2025-01-30 23:00:05 -0800

    First push

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    const user =req.body as {o:string};
+    res.send('Hello, TypeScript with Express!');
+
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
