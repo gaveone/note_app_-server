912bfde445b45fc390354c431083a72b98fda4fb
“gave” <adeun2020@gmail.com>
2025-01-30 23:04:23 -0800

    First push

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
     const user =req.body as {o:string};
